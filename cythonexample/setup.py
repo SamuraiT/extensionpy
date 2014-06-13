63a7fe16bbbbac543bee36312c6d1da43a766f50
SamuraiT <t.yasukawa01@gmail.com>
2014-06-13 23:42:49 +0900

    add cython example files

---
+++ cythonexample/setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""
+Copyright 2014 Tatsuro Yasukawa.
+
+Distributed under the GNU General Public License at
+gnu.org/licenses/gpl.html.
+"""
+
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+hello_modules = [Extension("hellocython", ["hellocython.pyx"])]
+
+setup(
+    name = 'Hello cython module',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = hello_modules
+)
+
