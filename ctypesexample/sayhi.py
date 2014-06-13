c01c37e5b3dae00efe422880ecb4ae62186bf7ef
SamuraiT <t.yasukawa01@gmail.com>
2014-06-13 23:44:12 +0900

    add cytpes example

---
+++ ctypesexample/sayhi.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""
+Copyright 2014 Tatsuro Yasukawa.
+
+Distributed under the GNU General Public License at
+gnu.org/licenses/gpl.html.
+"""
+import ctypes
+
+def main():
+    say = ctypes.CDLL('./hellotypes.so')
+    say.hello()
+
+if __name__ == '__main__':
+    main()
+
